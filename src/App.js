4c9ae7fcd89855687cd384b6d4e73cf7be758217
Mike Boring <shekinah999@msn.com>
2020-09-29 09:06:43 -0600

    Successfully talking to Api and displaying data on front end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/api/homepage/")
+      .then((res) => res.json())
+      .then((data) => this.setState({ posts: data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ghost Post</h1>
+        {this.state.posts.map((p) => (
+          <div key={p.id}>
+            <ul>
+              <li>Post Type: {p.post_type}</li>
+              <li>Post: {p.post_text}</li>
+              <li>Up Votes: {p.up_votes}</li>
+              <li>Down Votes: {p.down_votes}</li>
+              <li>Submission Time: {p.submission_time}</li>
+              <li>Last Updated: {p.last_updated}</li>
+              <li>Total Votes: {p.total_votes}</li>
+              <br />
+            </ul>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
