39664dda7e740685e8021126502dcf8570fa54c1
Mike Boring <shekinah999@msn.com>
2020-09-30 09:13:13 -0600

    Routing working

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
 import React from "react";
 import "./App.css";
+import AllPosts from "./AllPosts";
+import Boasts from "./Boasts";
+import Roasts from "./Roasts";
+import Popular from "./Popular";
+import CreatePost from "./CreatePost";
+import { Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-    };
-  }
-
-  componentDidMount() {
-    fetch("http://127.0.0.1:8000/api/homepage/")
-      .then((res) => res.json())
-      .then((data) => this.setState({ posts: data }));
-  }
-
   render() {
     return (
       <div>
-        <h1>Ghost Post</h1>
-        {this.state.posts.map((p) => (
-          <div key={p.id}>
-            <ul>
-              <li>Post Type: {p.post_type}</li>
-              <li>Post: {p.post_text}</li>
-              <li>Up Votes: {p.up_votes}</li>
-              <li>Down Votes: {p.down_votes}</li>
-              <li>Submission Time: {p.submission_time}</li>
-              <li>Last Updated: {p.last_updated}</li>
-              <li>Total Votes: {p.total_votes}</li>
-              <br />
-            </ul>
-          </div>
-        ))}
+        <Switch>
+          <Route exact path="/" render={(props) => <AllPosts />} />
+          <Route exact path="/boasts" render={(props) => <Boasts />} />
+          <Route exact path="/roasts" render={(props) => <Roasts />} />
+          <Route exact path="/popular" render={(props) => <Popular />} />
+          <Route exact path="/create" render={(props) => <CreatePost />} />
+        </Switch>
       </div>
     );
   }
---
+++ src/Popular.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/api/homepage/Highest_Rated/")
+      .then((res) => res.json())
+      .then((data) => this.setState({ posts: data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <br />
+        <div className="menu-links">
+          <Link to="/">
+            <button>All Posts</button>
+          </Link>
+          <Link to="/boasts">
+            <button>All Boasts</button>
+          </Link>
+          <Link to="/roasts">
+            <button>All Roasts</button>
+          </Link>
+          <Link to="/popular">
+            <button>Most Popular</button>
+          </Link>
+          <Link to="/create">
+            <button>Create Post</button>
+          </Link>
+        </div>
+        <div className="main">
+          <h1>Ghost Post</h1>
+          {this.state.posts.map((p) => (
+            <div key={p.id}>
+              <h3>{p.post_type.toUpperCase()}</h3>
+              <ul>
+                <li>Id: {p.id}</li>
+                <li>Post: {p.post_text}</li>
+                <li>Vote Score: {p.total_votes}</li>
+                <li>Submission Time: {p.submission_time}</li>
+                <li>Last Updated: {p.last_updated}</li>
+                <br />
+                <button>UP-{p.up_votes}</button>&nbsp;
+                <button>DOWN-{p.down_votes}</button>
+                <br />
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Popular;
